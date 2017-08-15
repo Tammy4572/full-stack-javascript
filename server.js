4cba07615acfb8d7515d2c8cdbfa966e554038d0
Tammy Castro <tammycastro@gmail.com>
2017-08-15 19:27:28 -0400

    updated config.js and added server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+import config from './config';
+import apiRouter from './api';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (request, response) => {
+     response.render('index', {
+          content: 'Hello Express and <em>EJS</em>!'
+     });
+});
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+     console.info('Express listening on port ', config.port);
+});
